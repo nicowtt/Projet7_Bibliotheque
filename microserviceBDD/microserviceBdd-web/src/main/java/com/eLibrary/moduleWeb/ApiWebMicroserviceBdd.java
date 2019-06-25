6e55bf962ce17a48cfb1456bde2a326aa8e241ab
nicow <nicobod31@gmail.com>
2019-06-25 10:01:17 +0200

    microserviceBDD update
    Improvement on dependency

--- microserviceBDD/microserviceBdd-web/src/main/java/com/eLibrary/moduleWeb/ApiWebMicroserviceBdd.java
+++ microserviceBDD/microserviceBdd-web/src/main/java/com/eLibrary/moduleWeb/ApiWebMicroserviceBdd.java
@@ -1,7 +1,6 @@
 package com.eLibrary.moduleWeb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,10 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.eLibrary"})
 @EnableJpaRepositories("com.eLibrary")
 @EntityScan("com.eLibrary")
-public class ModuleWebApplication {
+public class ApiWebMicroserviceBdd {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModuleWebApplication.class, args);
+		SpringApplication.run(ApiWebMicroserviceBdd.class, args);
 	}
 
 }
--- microserviceBDD/microserviceBdd-web/src/test/java/com/eLibrary/moduleWeb/ApiWebMicroserviceBddTests.java
+++ microserviceBDD/microserviceBdd-web/src/test/java/com/eLibrary/moduleWeb/ApiWebMicroserviceBddTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ModuleWebApplicationTests {
+public class ApiWebMicroserviceBddTests {
 
 	@Test
 	public void contextLoads() {
