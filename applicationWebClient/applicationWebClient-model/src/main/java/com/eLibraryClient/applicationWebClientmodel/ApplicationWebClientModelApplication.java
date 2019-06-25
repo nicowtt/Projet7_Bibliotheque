d1cddd95b4fa3ddb6225c0f85c8e8d8277398b97
nicow <nicobod31@gmail.com>
2019-06-25 10:45:28 +0200

    applicationWebClient created
    - applicationWebClient-business created
    - applicationWebClient-model created
    - applicationWebClient-proxies created
    - applicationWebClient-web created

---
+++ applicationWebClient/applicationWebClient-model/src/main/java/com/eLibraryClient/applicationWebClientmodel/ApplicationWebClientModelApplication.java
@@ -0,0 +1,13 @@
+package com.eLibraryClient.applicationWebClientmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationWebClientModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationWebClientModelApplication.class, args);
+	}
+
+}
---
+++ applicationWebClient/applicationWebClient-proxies/src/main/java/com/eLibraryClient/applicationWebClientproxies/ApplicationWebClientProxiesApplication.java
@@ -0,0 +1,13 @@
+package com.eLibraryClient.applicationWebClientproxies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationWebClientProxiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationWebClientProxiesApplication.class, args);
+	}
+
+}
