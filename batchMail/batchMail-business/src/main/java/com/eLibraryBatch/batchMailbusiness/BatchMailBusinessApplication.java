9f23d4ce85fb9bd2fbce553bbc09d073b33973e3
nicowtt <nicobod31@gmail.com>
2019-07-18 12:26:51 +0200

    application batchMail created
    multi module created:
    - business
    - model
    - proxies

---
+++ batchMail/batchMail-business/src/main/java/com/eLibraryBatch/batchMailbusiness/BatchMailBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.eLibraryBatch.batchMailbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchMailBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchMailBusinessApplication.class, args);
+	}
+
+}
