caf7864d0cf4e332594e1f401705d03c76df3a6a
nicow <nicobod31@gmail.com>
2019-07-29 15:47:57 +0200

    improvement on externalt application.properties
    Environnement variable needed (windows, linux, mac) CONF_DIR with path of file application-gmail.properties before lunch .jar

--- batchMail/batchMail-business/src/main/java/com/eLibraryBatch/batchMailbusiness/config/MailConfig.java
+++ batchMail/batchMail-business/src/main/java/com/eLibraryBatch/batchMailbusiness/config/MailConfig.java
@@ -10,7 +10,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-@PropertySource(name = "myProperties", value = "application-gmail.properties")
+@PropertySource(name = "myProperties", value = "file:${CONF_DIR}/application-gmail.properties")
 public class MailConfig {
 
     @Value("${host}")
